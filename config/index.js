18adbc3a0a80a4ff76cf598d88fd5d9420a73edf
Eliyar Mamatjan <eliyar_m@hotmail.com>
2017-10-10 23:13:47 -0400

    Production build

--- config/index.js
+++ config/index.js
@@ -7,8 +7,8 @@ const path = require('path')
 module.exports = {
     build: {
         env: require('./prod.env'),
-        index: path.resolve(__dirname, '../dist/index.html'),
-        assetsRoot: path.resolve(__dirname, '../dist'),
+        index: path.resolve(__dirname, 'index.html'),
+        assetsRoot: path.resolve(__dirname, './'),
         assetsSubDirectory: 'static',
         assetsPublicPath: './',
         productionSourceMap: true,
