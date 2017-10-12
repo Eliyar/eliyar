2bcf43cc620f0019d3728362dbfeee7b2c5170c4
Eliyar Mamatjan <eliyar_m@hotmail.com>
2017-10-12 16:06:38 -0400

    Updated the design of SliderModal

--- src/components/SliderModal.vue
+++ src/components/SliderModal.vue
@@ -1,18 +1,25 @@
 <template>
 	<div>
-		<div v-if="modalShown" class="slider-modal-header d-flex justify-content-center align-items-center">
-			<div class="logo-block d-none d-md-flex justify-content-center align-items-center">
-				<div class="logo"></div>
-				<div>Project Name</div>
+		<div v-if="modalShown" class="slider-modal-header container-fluid d-flex justify-content-start align-items-center">
+			<div class="row">
+				<div class="logo-block d-none d-md-flex justify-content-center align-items-center">
+					<div class="logo"></div>
+					<div>Project Name</div>
+				</div>
+				<div class="caption-block col d-flex align-items-center"><span>This is the caption</span></div>
+				<div class="thumbnail-block col-12 col-lg d-flex justify-content-lg-end justify-content-start align-items-center">
+					<div class="thumbnail" v-for="{ thumbnail, index } in 4" :key="index"></div>
+				</div>
+				<div class="close-block d-flex justify-content-center align-items-center" @click="onCloseModal">
+					<i class="material-icons">close</i>
+				</div>
 			</div>
-			<div class="caption">This is the caption</div>
-			<i class="close-block material-icons" @click="onCloseModal">close</i>
 		</div>
 		<b-modal id="slider-modal" ref="sliderModal" size="lg" class="d-flex" @shown="onModalShown" @hide="onModalHide">
 			<!-- <img src="http://res.cloudinary.com/dspnhpwnp/image/upload/v1507763235/sample.jpg" alt="This is the caption" class="img-fluid"> -->
 			<!-- <a href="http://res.cloudinary.com/dspnhpwnp/image/upload/v1507763664/Test%20Folder/Test_Image.jpg" target="_blank">
-				<img src="http://res.cloudinary.com/dspnhpwnp/image/upload/v1507763664/Test%20Folder/Test_Image.jpg" alt="This is the caption" class="img-fluid">
-			</a> -->
+												<img src="http://res.cloudinary.com/dspnhpwnp/image/upload/v1507763664/Test%20Folder/Test_Image.jpg" alt="This is the caption" class="img-fluid">
+											</a> -->
 			<a :href="currentImage" target="_blank">
 				<img :src="currentImage" alt="This is the caption" class="img-fluid">
 			</a>
@@ -34,7 +41,9 @@
 			},
 			onModalHide() {
 				this.modalShown = false;
-				this.$router.push({ name: 'Designs' })
+				this.$router.push({
+					name: 'Designs'
+				})
 			},
 			onCloseModal() {
 				this.$refs.sliderModal.hide();
@@ -43,15 +52,14 @@
 		mounted() {
 			this.$refs.sliderModal.show();
 		}
-
+	
 	}
 </script>
 
 <style lang="scss">
 	.slider-modal {
 		&-header {
-			height: 60px;
-			padding: 0 24px;
+			padding: 0;
 			position: fixed;
 			top: 0;
 			left: 0;
@@ -60,43 +68,82 @@
 			font-weight: 500;
 			background: rgba(0, 0, 0, 0.8);
 			color: white;
+			.row {
+				width: 100%;
+				margin: 0;
+			}
 			.logo {
 				&-block {
-					position: absolute;
-					left: 24px;
-					top: 14px;
+					height: 80px;
+					padding: 0 16px;
+					background: rgba(0, 0, 0, 0.2);
 				}
-				min-width: 34px;
-				min-height: 34px;
+				min-width: 44px;
+				min-height: 44px;
 				margin-right: 16px;
 				background: white;
 				box-shadow: 0 2px 4px 0 rgba(0, 0, 0, 0.07);
 				border-radius: 2px;
 			}
-			.close-block {
-				position: absolute;
-					right: 24px;
-					top: 18px;
-					&:hover {
-						cursor: pointer;
+			.thumbnail {
+				&-block {
+					height: 80px;
+					padding: 0 16px;
+					@media (max-width: 992px) {
+						order: 100;
 					}
+				}
+				width: 64px;
+				height: 44px;
+				margin-right: 16px;
+				background: white;
+				box-shadow: 0 2px 4px 0 rgba(0, 0, 0, 0.07);
+				border-radius: 2px;
+				opacity: 0.7;
+				&:last-child {
+					margin: 0;
+				}
+				&.active {
+					opacity: 1;
+				}
+				&.active,
+				&:hover {
+					opacity: 1;
+					cursor: pointer;
+				}
+			}
+			.caption-block {
+				height: 80px;
+				padding: 0 16px;
+			}
+			.close-block {
+				width: 80px;
+				height: 80px;
+				padding: 0 16px;
+				background: rgba(0, 0, 0, 0.2);
+				&:hover {
+					cursor: pointer;
+				}
 			}
 		}
 	}
 	
 	.modal {
 		top: 0;
-		padding-top: 100px;
+		padding-top: calc(80px + 16px);
+		@media (max-width: 992px) {
+			order: 100;
+			padding-top: calc(80px * 2 + 16px);
+		}
 		&-header,
 		&-footer {
 			display: none;
 		}
 		&-dialog {
 			width: 100%;
-			max-width: 1100px;
+			max-width: 1440px;
 			height: 0;
 			margin: auto;
-			margin-bottom: 100px;
 		}
 		&-content {
 			height: 100%;
@@ -105,10 +152,10 @@
 		}
 		&-body {
 			padding: 0;
-			margin-bottom: 100px;
+			margin: 0 16px 16px 16px;
 			img {
 				width: 100%;
-				border-radius: .3rem;
+				border-radius: 2px;
 			}
 		}
 		&-backdrop {
