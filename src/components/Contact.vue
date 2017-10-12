b09f2636fc3708180335fdc2d29ba10a2584640b
Eliyar Mamatjan <eliyar_m@hotmail.com>
2017-10-12 19:45:01 -0400

    Updated the design of Contact component

--- src/components/Contact.vue
+++ src/components/Contact.vue
@@ -2,6 +2,15 @@
 	<div class="wrapper">
 		<div class="content">
 			<app-navigation :page="'Contact'"></app-navigation>
+			<h4 v-if="submitted">Thank you for your message.</h4>
+			<b-form @submit="onSubmit">
+				<div class="d-flex">
+					<b-form-input type="text" class="margin-right-24" v-model="form.name" required placeholder="Name" :disabled="submitted"></b-form-input>
+					<b-form-input type="email" v-model="form.email" required placeholder="Email" :disabled="submitted"></b-form-input>
+				</div>
+				<b-form-textarea type="text" class="height-200 margin-top-24" v-model="form.message" required placeholder="Message" :disabled="submitted"></b-form-textarea>
+				<b-button type="submit" :disabled="submitted">Submit</b-button>
+			</b-form>
 		</div>
 	</div>
 </template>
@@ -10,6 +19,23 @@
 	import Navigation from '@/components/Navigation'
 	
 	export default {
+		data() {
+			return {
+				form: {
+					email: '',
+					name: '',
+					message: ''
+				},
+				submitted: false
+			}
+		},
+		methods: {
+			onSubmit(ev) {
+				ev.preventDefault();
+				console.log('form values:', this.form);
+				this.submitted = true;
+			}
+		},
 		components: {
 			'app-navigation': Navigation,
 		}
@@ -26,5 +52,43 @@
 		margin: auto;
 		padding: 16px;
 		text-align: left;
+		h4 {
+			margin-top: 16px;
+			font-family: "Didot", "sans-serif", Serif;
+		}
+		form {
+			max-width: 500px;
+			margin-top: 44px;
+			.form-control {
+				height: 44px;
+				border: 0;
+				border-radius: 2px;
+				box-shadow: 0 2px 4px 0 rgba(0, 0, 0, 0.07);
+				&.margin-right-24 {
+					margin-right: 24px;
+				}
+				&.margin-top-24 {
+					margin-top: 24px;
+				}
+				&.height-200 {
+					min-height: 200px;
+					max-height: 300px;
+				}
+			}
+		}
+		button {
+			height: 44px;
+			margin-top: 44px;
+			padding-left: 16px;
+			padding-right: 16px;
+			border-radius: 2px;
+			border: 0;
+			background: #333333;
+			text-transform: uppercase;
+			box-shadow: 0 2px 4px 0 rgba(0, 0, 0, 0.07);
+			&:hover {
+				cursor: pointer;
+			}
+		}
 	}
 </style>
