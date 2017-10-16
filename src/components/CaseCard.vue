a0bbb0f7ab5f44b742a620452afa7bbb957cae36
Eliyar Mamatjan <eliyar_m@hotmail.com>
2017-10-15 23:07:33 -0400

    Updated CaseCard component

--- src/components/CaseCard.vue
+++ src/components/CaseCard.vue
@@ -1,7 +1,8 @@
 <template>
 	<div>
 		<div class="image-wrapper" @click="onImageClick(portfolio)" :style="{ 'background-image': portfolio.project.thumbnailUrl ? 'url(' + portfolio.project.thumbnailUrl + ')' : '' }">
-			<div v-if="projectViews" class="view-count position-bottom-right d-flex align-items-center">
+			<div v-if="source === 'designs'" class="sketch-logo" style="background:url(https://res.cloudinary.com/dspnhpwnp/image/upload/v1508122428/cJ9QJmkB/sketch-logo.png) center / cover no-repeat;"></div>
+			<div v-if="projectViews" class="view-count color-black position-bottom-right d-flex align-items-center">
 				<i class="material-icons">visibility</i>
 				<span>{{ projectViews }}</span>
 			</div>
@@ -65,6 +66,15 @@
 			background-size: cover;
 			background-position: center;
 			background-repeat: no-repeat;
+			&:after {
+				content: '';
+				position: absolute;
+				top: 0;
+				right: 0;
+				bottom: 0;
+				left: 0;
+				background: rgba(0, 0, 0, 0.04);
+			}
 			&:hover {
 				cursor: pointer;
 			}
@@ -88,11 +98,11 @@
 	}
 	
 	.name {
+		font-size: 14px;
 		font-weight: 500;
 	}
 	
 	.action {
-	
 		color: #757575;
 		font-size: 14px;
 		font-weight: 500;
@@ -100,5 +110,13 @@
 		&:hover {
 			cursor: pointer;
 		}
+	}
+
+	.sketch-logo {
+		width: 18px;
+		height: 18px;
+		position: absolute;
+		bottom: 8px;
+		left: 8px;
 	}
 </style>
