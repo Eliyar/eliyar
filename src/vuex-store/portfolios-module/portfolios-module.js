d64298a9104c7f52fd19674bbcd2a51b0cdd92c7
Eliyar Mamatjan <eliyar_m@hotmail.com>
2017-10-15 21:00:56 -0400

    Linked up socialseek studio portfolio assets

--- src/vuex-store/portfolios-module/portfolios-module.js
+++ src/vuex-store/portfolios-module/portfolios-module.js
@@ -2,43 +2,83 @@ const state = {
     portfolios: [{
         project: {
             id: 'R3bO6iky',
-            name: 'Project A',
+            name: 'Socialseek Studio',
             url: 'https://google.ca',
-            logo: 'logo_url',
-            thumbnail: 'thumbnail_url',
+            logoUrl: 'https://res.cloudinary.com/dspnhpwnp/image/upload/v1508027763/R3bO6iky/j44bvWMfSK84YPvJ.svg',
+            thumbnailUrl: 'http://res.cloudinary.com/dspnhpwnp/image/upload/v1508024884/R3bO6iky/I2EIxBiS7kEmnegs.png',
+            showProject: true,
+            showDesigns: true
         },
         assets: [{
             id: 'H1FxAsQl',
-            url: 'https://res.cloudinary.com/dspnhpwnp/image/upload/v1507765101/Test%20Folder/Test_Image.jpg',
-            caption: 'This is the caption'
+            imageUrl: 'https://res.cloudinary.com/dspnhpwnp/image/upload/v1508114223/R3bO6iky/Xo8QIds1ITcyBaXQ.jpg',
+            thumbnailUrl: 'https://res.cloudinary.com/dspnhpwnp/image/upload/c_limit,h_100,w_150/v1508114223/R3bO6iky/Xo8QIds1ITcyBaXQ.jpg',
+            caption: 'Sign In to Socialseek Studio'
         }, {
             id: 'pTInCTEj',
-            url: 'https://res.cloudinary.com/dspnhpwnp/image/upload/v1507765101/Test%20Folder/Test_Image_2.jpg',
+            imageUrl: 'http://res.cloudinary.com/dspnhpwnp/image/upload/v1508024884/R3bO6iky/I2EIxBiS7kEmnegs.png',
+            thumbnailUrl: 'http://res.cloudinary.com/dspnhpwnp/image/upload/t_media_lib_thumb/v1508024884/R3bO6iky/I2EIxBiS7kEmnegs.png',
             caption: 'This is the other caption'
+        }, {
+            id: 'AMzwICyC',
+            imageUrl: 'https://res.cloudinary.com/dspnhpwnp/image/upload/v1508024877/R3bO6iky/GvhlQJLsaY1Nj5zE.png',
+            thumbnailUrl: 'http://res.cloudinary.com/dspnhpwnp/image/upload/t_media_lib_thumb/v1508024877/R3bO6iky/GvhlQJLsaY1Nj5zE.png',
+            caption: 'Content builder'
+        }, {
+            id: 'iG4hfgqI',
+            imageUrl: 'https://res.cloudinary.com/dspnhpwnp/image/upload/v1508024883/R3bO6iky/WXcjS2eTVDUJLf2E.png',
+            thumbnailUrl: 'http://res.cloudinary.com/dspnhpwnp/image/upload/t_media_lib_thumb/v1508024883/R3bO6iky/WXcjS2eTVDUJLf2E.png',
+            caption: 'Media catalogue'
+        }, {
+            id: 'YBhOGcQX',
+            imageUrl: 'https://res.cloudinary.com/dspnhpwnp/image/upload/v1508024878/R3bO6iky/ETp10t7xHi3qnpY2.png',
+            thumbnailUrl: 'http://res.cloudinary.com/dspnhpwnp/image/upload/t_media_lib_thumb/v1508024878/R3bO6iky/ETp10t7xHi3qnpY2.png',
+            caption: 'Products catalogue'
+        }, {
+            id: 'B73q7EVL',
+            imageUrl: 'https://res.cloudinary.com/dspnhpwnp/image/upload/v1508024877/R3bO6iky/cQIvEy7PSurBbxLa.png',
+            thumbnailUrl: 'http://res.cloudinary.com/dspnhpwnp/image/upload/t_media_lib_thumb/v1508024877/R3bO6iky/cQIvEy7PSurBbxLa.png',
+            caption: 'Analytics'
+        }, {
+            id: 'NGkGWJo8',
+            imageUrl: 'https://res.cloudinary.com/dspnhpwnp/image/upload/v1508029958/R3bO6iky/kpsgCCJ2XzX5gCUh.png',
+            thumbnailUrl: 'http://res.cloudinary.com/dspnhpwnp/image/upload/t_media_lib_thumb/v1508029958/R3bO6iky/kpsgCCJ2XzX5gCUh.png',
+            caption: 'Billing Page'
+        }, {
+            id: 'YZTnAI1J',
+            imageUrl: 'https://res.cloudinary.com/dspnhpwnp/image/upload/v1508024875/R3bO6iky/f7eZxmTeGcYk68YK.png',
+            thumbnailUrl: 'http://res.cloudinary.com/dspnhpwnp/image/upload/t_media_lib_thumb/v1508024875/R3bO6iky/f7eZxmTeGcYk68YK.png',
+            caption: 'Team Page'
         }]
     }, {
         project: {
             id: 'N1cp5waz',
             name: 'Project B',
             url: 'https://youtube.ca',
-            logo: 'logo_url',
-            thumbnail: 'thumbnail_url',
+            logoUrl: null,
+            thumbnailUrl: null,
+            showProject: true,
+            showDesigns: true
         }
     }, {
         project: {
             id: 'nEOjTaTX',
             name: 'Project C',
             url: 'https://twitter.com',
-            logo: 'logo_url',
-            thumbnail: 'thumbnail_url',
+            logoUrl: null,
+            thumbnailUrl: null,
+            showProject: true,
+            showDesigns: true
         }
     }, {
         project: {
             id: 'qYQb30qJ',
             name: 'Project D',
             url: 'https://facebook.com',
-            logo: 'logo_url',
-            thumbnail: 'thumbnail_url',
+            logoUrl: null,
+            thumbnailUrl: null,
+            showProject: true,
+            showDesigns: true
         }
     }]
 }
